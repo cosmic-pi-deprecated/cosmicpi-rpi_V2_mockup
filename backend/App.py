87d224f4a81e4be00f00e08ce3c71216b6b0a433
Hendrik Borras <hendrikborras@web.de>
2017-08-08 09:43:34 +0200

    Implemented basic configuration for location provider and UI

--- backend/App.py
+++ backend/App.py
@@ -7,21 +7,40 @@ sys.path.append('.')
 import time
 import sensors
 import detectors
+import configparser
+
+
+# settings files
+CONFIG_FILE = "CosmicPi.config"
+IMU_SETTINGS_FILE = "IMU_settings"
+
+# read configuration
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE)
+_enable_GPS = config.getboolean("Location_Provider", "enable_GPS")
+_enable_geoIP = config.getboolean("Location_Provider", "enable_geoIP")
+_enable_UI = config.getboolean("UI", "enable_UI")
 
 
 # start UI
-StaticContentServer.async_start(port=Constants.static_content_port)
-WebSocket.async_start(port=Constants.web_socket_port)
+if _enable_UI:
+    StaticContentServer.async_start(port=Constants.static_content_port)
+    WebSocket.async_start(port=Constants.web_socket_port)
 
 #DummyDataGenerator.async_start()
 
 # set up the IMU
-IMU_SETTINGS_FILE = "IMU_settings"
 imu = sensors.IMU_Reader(IMU_SETTINGS_FILE)
 
-# set up and start the GPS
-#location = sensors.GPS_location_provider()
-location = sensors.IP_location_provider()
+# set up and start the location provider
+if _enable_geoIP and _enable_GPS:
+    location = sensors.Combined_location_provider()
+elif _enable_GPS:
+    location = sensors.GPS_location_provider()
+elif _enable_geoIP:
+    location = sensors.IP_location_provider()
+else:
+    raise RuntimeError("No location provider was specified! Please enable at least one in: " + str(CONFIG_FILE))
 
 # start our detector
 detector = detectors.simulated_detector()
