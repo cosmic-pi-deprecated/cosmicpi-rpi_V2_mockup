480ba1e1c86596755c4f87435c9ecb453bc09cc5
Hendrik Borras <hendrikborras@web.de>
2017-08-09 15:16:01 +0200

    Added the startup of the AP

--- backend/App.py
+++ backend/App.py
@@ -8,6 +8,7 @@ import time
 import sensors
 import detectors
 import configparser
+import subprocess
 
 
 # settings files
@@ -17,27 +18,38 @@ IMU_SETTINGS_FILE = "IMU_settings"
 # read configuration
 config = configparser.ConfigParser()
 config.read(CONFIG_FILE)
-_enable_GPS = config.getboolean("Location_Provider", "enable_GPS")
-_enable_geoIP = config.getboolean("Location_Provider", "enable_geoIP")
-_enable_UI = config.getboolean("UI", "enable_UI")
+_GPS_enable = config.getboolean("Location Provider", "enable_GPS")
+_geoIP_enable = config.getboolean("Location Provider", "enable_geoIP")
+_UI_enable = config.getboolean("UI", "enable_UI")
+_AP_enable = config.getboolean("Access Point", "enable")
+_AP_name = config.get("Access Point", "name")
+_AP_pw = config.get("Access Point", "password")
 
+
+# start the AP
+if _AP_enable:
+    cmd_list = ['sudo', '/usr/bin/create_ap', 'wlan0', 'eth0', _AP_name, _AP_pw]
+    try:
+        _AP_process = subprocess.Popen(cmd_list)
+    except:
+        print("Unexpected error while starting Access Point: ", end='')
+        print(sys.exc_info()[0])
 
 # start UI
-if _enable_UI:
+if _UI_enable:
     StaticContentServer.async_start(port=Constants.static_content_port)
     WebSocket.async_start(port=Constants.web_socket_port)
 
-#DummyDataGenerator.async_start()
 
 # set up the IMU
 imu = sensors.IMU_Reader(IMU_SETTINGS_FILE)
 
 # set up and start the location provider
-if _enable_geoIP and _enable_GPS:
+if _geoIP_enable and _GPS_enable:
     location = sensors.Combined_location_provider()
-elif _enable_GPS:
+elif _GPS_enable:
     location = sensors.GPS_location_provider()
-elif _enable_geoIP:
+elif _geoIP_enable:
     location = sensors.IP_location_provider()
 else:
     raise RuntimeError("No location provider was specified! Please enable at least one in: " + str(CONFIG_FILE))
