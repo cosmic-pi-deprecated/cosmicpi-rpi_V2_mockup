0cc6d9425ac04175ef602145d5349a46056a8bc4
Darko Lukic <lukicdarkoo@gmail.com>
2017-08-16 11:09:49 +0000

    Add wifi setting event

--- backend/Constants.py
+++ backend/Constants.py
@@ -1,4 +1,12 @@
 class Constants(object):
-    allowed_events = ('on_temperature', 'on_pressure', 'on_magnetism', 'on_location', 'on_combined_event_count', 'on_serial', 'set_ADC_readings')
+    allowed_events = (
+        'on_temperature',
+        'on_pressure',
+        'on_magnetism',
+        'on_location',
+        'on_combined_event_count',
+        'on_serial',
+        'set_ADC_readings',
+        'on_wifi_setting')
     web_socket_port = 9000
     static_content_port = 8080
