f60247fb03033f17fce44131a040fabd0180a23e
Darko Lukic <lukicdarkoo@gmail.com>
2017-08-17 10:42:11 +0800

    Fix translation keys

--- web/components/Settings.js
+++ web/components/Settings.js
@@ -3,24 +3,24 @@ window.Settings = Vue.component('settings', {
     <div class="component-content">
 
 		  <form class="form-horizontal">
-          <h3>Wifi Settings</h3>
+          <h3>{{ $t('Wifi Settings') }}</h3>
           <hr />
 
           <div class="form-group">
-            <label class="control-label col-sm-2" for="email">SSID:</label>
+            <label class="control-label col-sm-2" for="email">{{ $t('SSID') }}:</label>
             <div class="col-sm-10">
-              <input type="text" class="form-control" ref="ssid" placeholder="Enter SSID">
+              <input type="text" class="form-control" ref="ssid" v-bind:placeholder="$t('Enter SSID')">
             </div>
           </div>
           <div class="form-group">
-            <label class="control-label col-sm-2" for="pwd">Password:</label>
+            <label class="control-label col-sm-2" for="pwd">{{ $t('Password') }}:</label>
             <div class="col-sm-10">
-              <input type="password" class="form-control" ref="password" id="pwd" placeholder="Enter password">
+              <input type="password" class="form-control" ref="password" id="pwd" v-bind:placeholder="$t('Enter password')">
             </div>
           </div>
           <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
-              <button type="submit" class="btn btn-primary" v-on:click.prevent="onConnect()">Connect</button>
+              <button type="submit" class="btn btn-primary" v-on:click.prevent="onConnect()">{{ $t('Connect') }}</button>
             </div>
           </div>
         </form>
